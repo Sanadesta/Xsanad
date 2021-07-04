15938a56caa54f520066ce94be9d1dc4a50da3a0
Sanadesta <86498606+Sanadesta@users.noreply.github.com>
2021-07-04 12:48:36 +0200

    Add files via upload

---
+++ SH.py
@@ -0,0 +1,24 @@
+import time
+import requests
+import sys as n
+import time as mm
+
+#TweakPY
+count = 0
+def slow(M):
+    for c in M + '\n':
+        n.stdout.write(c)
+        n.stdout.flush()
+        mm.sleep(1. / 200)
+p = requests.get('https://pastebin.com/raw/RZ5wsJ73').text
+slow(p)
+time.sleep(1)
+slow("\n- CHECKER TIKTOK VERSION 1.05\n")
+time.sleep(1)
+
+slow("ستظهر الان صفحة ويب للقناه حفاظاً على الحقوق ☢️\n Now a web page for the channel appears, in order to preserve the rights ☢️")
+
+import webbrowser
+webbrowser . open('https://t.me/Sanadesta_Pubg')
+time.sleep(4)
+print("sanadesta")
